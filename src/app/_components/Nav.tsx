f0b738a42fa7406346797d5771a1703135c0bca6
Muhammad Atif khan <22pwbcs0906@uetpeshawar.edu.pk>
2026-01-20 22:59:12 +0500

    login page

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,19 +1,25 @@
+import Link from "next/link"
+
 const Navbar = ()=>{
 return (
   <header className="bg-slate-900 text-white shadow-md">
     <div className="max-w-7xl mx-auto px-6 py-6 flex items-center justify-between">
-      <h1 className="text-xl md:text-2xl font-semibold tracking-wide">
+     <Link href='/'> <h1 className="text-xl md:text-2xl font-semibold tracking-wide">
         Contact Management App
-      </h1>
+      </h1></Link>
 
       <nav>
         <ul className="flex space-x-6 text-sm md:text-base">
+      <Link href={'/'}>
           <li className="cursor-pointer hover:text-sky-400 transition-colors">
             Home
           </li>
+      </Link>
+         <Link href={'/contacts'}>
           <li className="cursor-pointer hover:text-sky-400 transition-colors">
             Contacts
           </li>
+         </Link>
           <li className="cursor-pointer hover:text-sky-400 transition-colors">
             Add Contact
           </li>
